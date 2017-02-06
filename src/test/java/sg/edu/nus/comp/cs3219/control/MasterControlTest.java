57a00b60793d4c496883d847b3ca824e41f27276
See Loo Jane <muigek34@yahoo.com.sg>
2017-02-06 16:02:15 +0800

    Fixed bugs in test cases

--- src/test/java/sg/edu/nus/comp/cs3219/control/MasterControlTest.java
+++ src/test/java/sg/edu/nus/comp/cs3219/control/MasterControlTest.java
@@ -36,45 +36,45 @@ public class MasterControlTest {
 		input.add("Man of Steel");
 		
 		Set<String> requiredWords = new HashSet<>();
+		requiredWords.add("Day");
+		requiredWords.add("Tomorrow");
+		requiredWords.add("Fast");
+		requiredWords.add("Furious");
+		requiredWords.add("Man");
+		requiredWords.add("Steel");
 
 		List<String> result = master.run(input, ignoreWords, requiredWords);
-
+	
 		assertEquals(6, result.size());
-		assertEquals("Day after Tomorrow the", result.get(0));
-		assertEquals("Fast and Furious", result.get(1));
-		assertEquals("Furious Fast and", result.get(2));
-		assertEquals("Man of Steel", result.get(3));
-		assertEquals("Steel Man of", result.get(4));
-		assertEquals("Tomorrow the Day after", result.get(5));
+		assertEquals("Day after tomorrow the", result.get(0));
+		assertEquals("Fast and furious", result.get(1));
+		assertEquals("Furious fast and", result.get(2));
+		assertEquals("Man of steel", result.get(3));
+		assertEquals("Steel man of", result.get(4));
+		assertEquals("Tomorrow the day after", result.get(5));
 	}
 	
 	@Test
 	public void testExample2() {
 		Set<String> ignoreWords = new HashSet<>();
-		ignoreWords.add("is");
-		ignoreWords.add("the");
-		ignoreWords.add("of");
 		ignoreWords.add("and");
-		ignoreWords.add("as");
-		ignoreWords.add("a");
-		ignoreWords.add("after");
 
 		List<String> input = new ArrayList<>();
-		input.add("The day after tomorrow");
-		input.add("Fast and Furious");
-		input.add("Man of Steel");
+		input.add("Andy ander and hand");
 		
 		Set<String> requiredWords = new HashSet<>();
+		requiredWords.add("Andy");
+		requiredWords.add("ander");
+		requiredWords.add("hand");
 		
-		List<String> result = master.run(input, ignoreWords, requiredWords);
+		List<String> result = master.run(input, ignoreWords, requiredWords);	
 
-		assertEquals(6, result.size());
-		assertEquals("Day after tomorrow the", result.get(0));
-		assertEquals("Fast and Furious", result.get(1));
-		assertEquals("Furious Fast and", result.get(2));
-		assertEquals("Man of Steel", result.get(3));
-		assertEquals("Steel Man of", result.get(4));
-		assertEquals("Tomorrow the day after", result.get(5));
+		System.out.println(result);
+		
+		assertEquals(3, result.size());
+		assertEquals("Andy ander and hand", result.get(1));
+		assertEquals("Ander and hand andy", result.get(0));
+		assertEquals("Hand andy ander and", result.get(2));
 	}
 	
 	@Test
@@ -103,7 +103,7 @@ public class MasterControlTest {
 		assertEquals(3, result.size());
 		assertEquals("Day after tomorrow the", result.get(0));
 		assertEquals("Fast and Furious", result.get(1));
-		assertEquals("Man of Steel", result.get(3));
+		assertEquals("Man of Steel", result.get(2));
 	}
 }
 
