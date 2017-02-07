178ca49238dba45c7f0b2e809c0dfd4ae6eaf492
See Loo Jane <muigek34@yahoo.com.sg>
2017-02-07 16:32:32 +0800

    Implemented test cases for RequiredWordsFilterTest

--- src/test/java/sg/edu/nus/comp/cs3219/module/RequiredWordsFilterTest.java
+++ src/test/java/sg/edu/nus/comp/cs3219/module/RequiredWordsFilterTest.java
@@ -1,16 +1,38 @@
 package sg.edu.nus.comp.cs3219.module;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import sg.edu.nus.comp.cs3219.model.LineStorage;
+
 public class RequiredWordsFilterTest {
+	LineStorage shiftedLineStorage;
+	LineStorage afterFilterLineStorage;
+	RequiredWordsFilter filter;
+	
+	
 	@Before
 	public void setUp() {
-		// TODO
+		shiftedLineStorage = new LineStorage();
+		afterFilterLineStorage = new LineStorage();
+		filter = new RequiredWordsFilter(afterFilterLineStorage);
+		Set<String> words = new HashSet<>();
+		words.add("day");
+		filter.setRequiredWords(words);
+		shiftedLineStorage.addObserver(filter);
 	}
 
 	@Test
-	public void test() {
-		// TODO
+	public void test1() {
+		shiftedLineStorage.addLine("Day after Tomorrow the");
+		shiftedLineStorage.addLine("Tomorrow the Day after");
+		
+		assertEquals(1, afterFilterLineStorage.size());
+		assertEquals("day after Tomorrow the", afterFilterLineStorage.get(0).toString());
 	}
 }
